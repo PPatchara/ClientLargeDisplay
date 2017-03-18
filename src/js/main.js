a37868edf9d77c9915d0c8b5c67b9237922e5788
PATCHARA PATTANAKIMHUN <Justwyne@PATCHARAs-MacBook-Air.local>
2017-03-18 23:06:36 +0700

    The beginning of our story

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+// /* global ga:false */
+
+function log(topic, message) {
+    console.log('[' + topic + ']: ' + message);
+}
+
+(function (window, undefined) {
+
+    'use strict';
+
+    var document = window.document;
+
+}(window));
